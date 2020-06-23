78a0fe18c5b4cb76f8cd019a1bca1da3dcbe20f4
andrewsc-lwc <64260666+andrewsc-lwc@users.noreply.github.com>
2020-06-23 10:24:10 +0100

    Add files via upload

---
+++ ClassGUIs/v2/main.py
@@ -0,0 +1,30 @@
+from login import MyLogin
+from tkinter import *
+from tkinter import font
+
+root=Tk()
+root.geometry("300x300")
+root.title("Menu")
+myFont = font.Font(family="Comic Sans MS",size=22,weight="bold",slant="italic")
+backColour = "Green"
+fontColour = "Black"
+buttonColour = "Red"
+pwAttempts = 3
+
+def login():
+    loginSuccess.config(text="Login Pending")
+    loginButton.config(state="disabled")
+    f = Toplevel()
+    login1 = MyLogin(f,title="Login",geometry="500x500",backColour=backColour,
+                     font=myFont,pwAttempts=pwAttempts,fontColour=fontColour,buttonColour=buttonColour)
+    root.wait_window(f)
+    loginButton.config(state="normal")
+    loginSuccess.config(text=login1.infoText.get())
+    
+loginButton = Button(root,text="Login",command=login,font=myFont,fg=fontColour,bg=buttonColour)
+loginButton.grid()
+loginSuccess = Label(root,text="")
+loginSuccess.grid()
+
+root.mainloop()
+
